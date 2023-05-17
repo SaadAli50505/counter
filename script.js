c3bf8f6b93b080c30d5df2ecee39e933ace05f7a
Saad Ali <77667699+SaadAli50505@users.noreply.github.com>
2023-05-17 19:43:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+function decrement(){
+    var b =  parseInt(document.getElementById("grid-item-2").innerHTML);
+    b = b - 1;
+    document.getElementById("grid-item-2").innerHTML = b;
+
+}
+
+function increment(){
+    var b =  parseInt(document.getElementById("grid-item-2").innerHTML);
+    b = b + 1;
+    document.getElementById("grid-item-2").innerHTML = b;
+
+}
